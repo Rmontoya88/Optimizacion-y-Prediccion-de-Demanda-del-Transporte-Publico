9d09c757fdd907cf27d1b57663d512d842942c0d
Mónica Mendoza Morales <moniik3010@gmail.com>
2025-08-18 09:55:17 -0600

    ft:Agregada la tabla de Historial de tarifas y se inserto la data en la tabla

--- src/basedatos/gestor_basedatos.py
+++ src/basedatos/gestor_basedatos.py
@@ -23,12 +23,23 @@ class Pasajeros_TrenUrbano(Base):
     cantidadadultosmay = Column(SmallInteger)
     ingresos = Column(DECIMAL(10,2))
 
+class Tarifas_Historicas(Base):
+    __tablename__ = 'tarifas_historicas'
+    id = Column(Integer, primary_key=True,autoincrement=True)
+    servicio = Column(String)
+    recorrido = Column(String)
+    tarifaregular = Column(SmallInteger)
+    resolucion = Column(String)
+    fechapublicacionlagaceta = Column(Date)
+    expediente = Column(String)
+
 passw = quote_plus("LMtco26.08")
 engine = create_engine(f"postgresql+psycopg2://postgres:{passw}@localhost:5432/Incofer", echo=True)
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
-df_pasajeros = pd.read_csv( r"C:\Users\Fabiola\Documents\CUC\BigData\Programacion II\Optimizacion-y-Prediccion-de-Demanda-del-Transporte-Publico\data\raw\Tren_urbano_pasajeros.csv",encoding="utf-8-sig")
 
+#Pasajeros
+df_pasajeros = pd.read_csv( r"C:\Users\Fabiola\Documents\CUC\BigData\Programacion II\Optimizacion-y-Prediccion-de-Demanda-del-Transporte-Publico\data\raw\Tren_urbano_pasajeros.csv",encoding="utf-8-sig")
 df_pasajeros.columns =(
     df_pasajeros.columns
     .str.strip()
@@ -36,6 +47,7 @@ df_pasajeros.columns =(
     .str.replace(" ","",regex=True)
     .str.lower()
 )
+
 #Renombrar columnas
 df_pasajeros = df_pasajeros.rename(columns ={
     "codigoderuta":"codigoruta",
@@ -57,4 +69,25 @@ df_pasajeros['cantidadadultosmay'] = df_pasajeros['cantidadadultosmay'].fillna(0
 df_pasajeros.to_sql("pasajeros_trenurbano", engine, if_exists='append',index=False)
 #Verificar que se insertaran los datos
 df_test = pd.read_sql("Select * from pasajeros_trenurbano",engine)
-print(df_test)
+
+#Tarifas
+df_tarifas = pd.read_csv(r"C:\Users\Fabiola\Documents\CUC\BigData\Programacion II\Optimizacion-y-Prediccion-de-Demanda-del-Transporte-Publico\data\raw\Tarifas_históricas_de_tren.csv",encoding="utf-8-sig")
+df_tarifas.columns =(
+    df_tarifas.columns
+    .str.strip()
+    .map(lambda x: unidecode(x))
+    .str.replace(" ","",regex=True)
+    .str.lower()
+)
+
+df_tarifas = df_tarifas.rename(columns ={
+    "servicio":"servicio",
+    "recorrido":"recorrido",
+    "tarifaregular(c/)":"tarifaregular",
+    "resolucion":"resolucion",
+    "fechapublicacionlagaceta":"fechapublicacionlagaceta",
+    "expediente":"expediente"
+})
+df_tarifas.to_sql("tarifas_historicas", engine, if_exists='append',index=False)
+df_test1 = pd.read_sql("Select * from tarifas_historicas",engine)
+print(df_test1)
